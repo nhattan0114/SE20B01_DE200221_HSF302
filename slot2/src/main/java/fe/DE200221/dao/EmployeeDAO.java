04189fdd94ccf0fa80a27c45e493d8d9f848bf10
nhattan0114 <nhattan0114.fudn@gmail.com>
2026-09-14 20:16:50 +0700

    feat(dao): implement update method with merge in EmployeeDAO

--- slot2/src/main/java/fe/DE200221/dao/EmployeeDAO.java
+++ slot2/src/main/java/fe/DE200221/dao/EmployeeDAO.java
@@ -70,6 +70,25 @@ public class EmployeeDAO {
         }
     }
 
+    // ---------- UPDATE (TODO 0.6) ----------
+    public Employee update(Employee e) {
+        // e truyen vao co the dang DETACHED (lay tu findById() o mot EntityManager khac)
+        EntityManager em = emf.createEntityManager();
+        try {
+            em.getTransaction().begin();
+            Employee merged = em.merge(e); // merge() TRA VE mot entity MANAGED khac
+            em.getTransaction().commit();
+            return merged; // PHAI dung object nay tiep, khong dung "e" cu
+        } catch (RuntimeException ex) {
+            if (em.getTransaction().isActive()) em.getTransaction().rollback();
+            throw ex;
+        } finally {
+            em.close();
+        }
+    }
+
+
+
 
 
 
